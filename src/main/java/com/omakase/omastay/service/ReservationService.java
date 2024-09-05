01ea13d1a07d686b14239f202ae6f3b25d4acaa9
JuHyun <aajj9776@hanmail.net>
2024-09-05 10:49:03 +0900

    결제 완료

--- src/main/java/com/omakase/omastay/service/ReservationService.java
+++ src/main/java/com/omakase/omastay/service/ReservationService.java
@@ -1,14 +1,17 @@
 package com.omakase.omastay.service;
 
 import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.omakase.omastay.dto.PaymentDTO;
+import com.omakase.omastay.dto.ReservationDTO;
 import com.omakase.omastay.entity.Payment;
 import com.omakase.omastay.entity.enumurate.PayStatus;
 import com.omakase.omastay.mapper.PaymentMapper;
 import com.omakase.omastay.repository.PaymentRepository;
+import com.omakase.omastay.repository.ReservationRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -17,6 +20,9 @@ public class ReservationService {
 
     @Autowired
     private PaymentRepository paymentRepository;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
 
     @Transactional
     public PaymentDTO insertPaymentInfo(PaymentDTO payment) {
@@ -38,6 +44,10 @@ public class ReservationService {
 
         PaymentDTO dto = PaymentMapper.INSTANCE.toPaymentDTO(pay);
         return dto;
+    }
+
+    public ReservationDTO insertReservationInfo(int res) {
+        return null;
     }
 
 
