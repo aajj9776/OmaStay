aa85f8cb4f30e40ca1f181b2f087e94010e31c50
‘Haun’ <hawoon_143@naver.com>
2024-09-09 09:24:40 +0900

    호스트 공지사항 완료

--- src/main/resources/static/js/datepicker-search.js
+++ src/main/resources/static/js/datepicker-search.js
@@ -4,12 +4,10 @@
 //위에 있는거 같이 첨부해야됨
 $(document).ready(function () {
     var start = moment();
-    var end = moment().add(1, 'days');
+    var end = moment();
 
     function setDateRangePicker(start, end) {
-        var nights = end.diff(start, 'days');
-        var days = nights + 1;
-        var displayText = start.format('MM/DD') + " ~ " + end.format('MM/DD') + " (" + nights + "박 " + days + "일)";
+        var displayText = start.format('YYYY-MM-DD') + " ~ " + end.format('YYYY-MM-DD');
         $('#date-range-picker-input').val(displayText);
     }
 
@@ -24,8 +22,8 @@ $(document).ready(function () {
         },
         showDropdowns: true,
         autoApply: true,
-        minDate: moment(),
-        maxDate: moment().add(6, 'months'),
+        minDate: moment().add(-36, 'months'),
+        maxDate: moment(),
         timePicker: false,
         singleDatePicker: false,
         startDate: start,
@@ -34,5 +32,4 @@ $(document).ready(function () {
         setDateRangePicker(start, end);
     });
 
-    setDateRangePicker(start, end);
 });
