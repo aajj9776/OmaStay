fa6c2ddfa84c356fdbed9ad73ab98c77f5023416
‘Haun’ <hawoon_143@naver.com>
2024-09-19 11:54:17 +0900

    이상해요 멤버레파짓토리

--- src/main/java/com/omakase/omastay/repository/MemberRepository.java
+++ src/main/java/com/omakase/omastay/repository/MemberRepository.java
@@ -10,14 +10,18 @@ import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer>, MemberRepositoryCustom {
 
-    Member findByEmail(String email);
 
     Member findByMemberProfileEmail(String email);
 
-    List<Member> findByGrade(String grade);
+    @Query("SELECT m FROM Member m WHERE m.grade.gCate = :grade")
+    List<Member> findByGrade(@Param("grade") String grade);
+
+    @Query("SELECT m FROM Member m WHERE m.memberProfile.email = :email")
+    Member findByEmail(@Param("email") String email);
 }
