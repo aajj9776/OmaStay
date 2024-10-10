634f5eb0de96a8062168875b80f6406fde1235d1
SeungTaek <ogs00700@naver.com>
2024-10-10 17:24:08 +0900

    fix

--- src/main/java/com/omakase/omastay/repository/RecommendationRepository.java
+++ src/main/java/com/omakase/omastay/repository/RecommendationRepository.java
@@ -2,10 +2,10 @@ package com.omakase.omastay.repository;
 
 import com.omakase.omastay.entity.Recommendation;
 import com.omakase.omastay.repository.custom.RecommendationRepositoryCustom;
-
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 import com.omakase.omastay.entity.enumurate.HCate;
