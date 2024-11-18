cc5ce10e1a83345da1cb69ef4ad1eb4f11e6c4e8
dlflqhfl <165862583+dlflqhfl@users.noreply.github.com>
2024-11-18 18:25:41 +0900

    Update OmaStayApplication.java

--- src/main/java/com/omakase/omastay/OmaStayApplication.java
+++ src/main/java/com/omakase/omastay/OmaStayApplication.java
@@ -1,5 +1,6 @@
 package com.omakase.omastay;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,7 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class OmaStayApplication {
 
-    Dotenv dotenv = Dotenv.configure().load();
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
         System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
         System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
         System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
@@ -20,8 +23,7 @@ public class OmaStayApplication {
         System.setProperty("GOOGLE_CLIENT_ID", Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_ID")));
         System.setProperty("GOOGLE_CLIENT_SECRET", Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_SECRET")));
         System.setProperty("GOOGLE_REFRESH_TOKEN", Objects.requireNonNull(dotenv.get("GOOGLE_REFRESH_TOKEN")));
-
-    public static void main(String[] args) {
+        
         SpringApplication.run(OmaStayApplication.class, args);
 
     }
