afe8c44a81b6f265929cd2a5d9db1da1ecc3ca62
dlflqhfl <165862583+dlflqhfl@users.noreply.github.com>
2024-11-20 15:32:37 +0900

    Update WebCORSConfig.java

--- src/main/java/com/omakase/omastay/global/security/WebCORSConfig.java
+++ src/main/java/com/omakase/omastay/global/security/WebCORSConfig.java
@@ -9,7 +9,7 @@ public class WebCORSConfig implements WebMvcConfigurer { // CORS 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9090", "http://omastay.duckdns.org", "http://www.omastay.duckdns.org") // 특정 출처만 허용
+                .allowedOrigins("http://localhost:9090", "http://omastay.duckdns.org", "http://www.omastay.duckdns.org, https://www.omastay.duckdns.org, https://omastay.duckdns.org") // 특정 출처만 허용
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true); // 인증 정보 허용
